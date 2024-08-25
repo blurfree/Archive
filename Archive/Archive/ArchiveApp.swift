7b0831846172b62de75768a43b5d728c50c5b51c
KoSungmin <blur2521@gmail.com>
2024-08-26 02:33:22 +0900

    Create Project

---
+++ Archive/Archive/ArchiveApp.swift
@@ -0,0 +1,17 @@
+//
+//  ArchiveApp.swift
+//  Archive
+//
+//  Created by blurfree on 8/26/24.
+//
+
+import SwiftUI
+
+@main
+struct ArchiveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
